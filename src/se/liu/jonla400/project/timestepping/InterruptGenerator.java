4d8f3d2ac2a747666c0c8cab9e631164eec6038e
Jonatan Larsson <jonatanlarsson2001@gmail.com>
2022-02-19 20:28:20 +0100

    Clarify the contract of interrupt generators

--- src/se/liu/jonla400/project/timestepping/InterruptGenerator.java
+++ src/se/liu/jonla400/project/timestepping/InterruptGenerator.java
@@ -9,7 +9,9 @@ import java.util.Optional;
 public interface InterruptGenerator
 {
     /**
-     * Generates the next upcoming interrupt of a time step, if any
+     * Generates the next upcoming interrupt of a time step, if any.
+     * An implementation of this method is allowed to return an interrupt
+     * that occurs after the time step has ended, but is not expected to.
      * @param timeLeft The time left of the time step
      * @return The next upcoming interrupt of the time step, if any
      */
